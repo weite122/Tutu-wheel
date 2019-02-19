e15a0a3a3ec78bcd1f31538bb757e08788ddaa76
weite122 <949193531@qq.com>
2019-02-19 17:56:06 +0800

    row 和　col 的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="[`col-${span}`]" class="col">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "WheelCol",
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
