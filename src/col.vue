feb7c3bf731d02dbe08429463f625720495de680
weite122 <949193531@qq.com>
2019-02-21 02:48:35 +0800

    继续重构代码

--- src/col.vue
+++ src/col.vue
@@ -36,17 +36,25 @@
         }
       }
     },
+    methods:{
+      createClasses (obj, str = '') {
+        if (!obj) {return []}
+        let array = []
+        if (obj.span) { array.push(`col-${str}${obj.span}`) }
+        if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
+        return array
+      }
+    },
     computed: {
       colClass(){
         let {span, offset, ipad, narrowPc, pc, widePc} = this
-
+        let createClasses = this.createClasses
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          ... (ipad ? [`col-ipad-${ipad.span}`] : []),
-          ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-          ... (pc ? [`col-pc-${pc.span}`] : []),
-          ... (widePc ? [`col-wide-pc-${widePc.span}`] : [])
+          ...createClasses({span, offset}),
+          ...createClasses(ipad, 'ipad-'),
+          ...createClasses(narrowPc, 'narrow-pc-'),
+          ...createClasses(pc, 'pc-'),
+          ...createClasses(widePc, 'wide-pc-'),
         ]
       },
       colStyle() {
@@ -60,7 +68,7 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
     .col {
         $class-prefix: col-;
         @for $n from 1 through 24 {
@@ -88,7 +96,7 @@
                 }
             }
         }
-        @media (min-width: 769px) {
+        @media (min-width: 769px){
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -102,7 +110,7 @@
                 }
             }
         }
-        @media (min-width: 993px)  {
+        @media (min-width: 993px) {
             $class-prefix: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
