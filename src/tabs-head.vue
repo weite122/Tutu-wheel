7c3fcc25c45e4d0af209fb584939338bfdd81fb0
weite122 <949193531@qq.com>
2019-02-28 23:51:00 +0800

    完善 tabs 样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -12,9 +12,11 @@
   export default {
     name: "WheelTabsHead",
     inject: ['eventBus'],
-    created() {
+    mounted() {
       this.eventBus.$on('update:selected', (item, vm) =>{
-        console.log(vm.$el)
+        let {width, height, top, left} = vm.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.left = `${left}px`
       })
     }
   }
@@ -22,18 +24,26 @@
 
 <style lang="scss" scoped>
     $tab-height: 40px;
+    $border-color: #e4e7ed;
     .tabs-head {
         display: flex;
         height:$tab-height;
         justify-content: flex-start;
         background: #f5f7fa;
-        >.line {
+        position: relative;
+        border: 1px solid $border-color;
+        > .line {
             position: absolute;
             bottom: 0;
-            border-bottom: 1px solid #e4e7ed;
+            border-bottom: 1px solid #409EFF;
+            transition: all .3s;
         }
         > .actions-wrapper {
             margin-left: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 1em;
         }
     }
 </style>
