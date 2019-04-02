89442318e638be319e937d1472b84d6895f52e9c
weite122 <949193531@qq.com>
2019-04-02 16:59:23 +0800

    [A]添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,43 +3,46 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
-      <slot></slot>
-    </span>
+        <span ref="triggerWrapper" style="display: inline-block;">
+          <slot></slot>
+        </span>
     </div>
 </template>
 
 <script>
   export default {
     name: "WheelPopover",
-    data () {
+    data() {
       return {visible: false}
     },
     methods: {
-      positionContent () {
+      positionContent() {
         document.body.appendChild(this.$refs.contentWrapper)
         let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
         this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
       },
-      onClickDocument (e) {
+      onClickDocument(e) {
         if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
-        ) { return }
+        ) {return}
+        if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+        ) {return}
         this.close()
       },
-      open () {
+      open() {
         this.visible = true
         this.$nextTick(() => {
           this.positionContent()
           document.addEventListener('click', this.onClickDocument)
         })
       },
-      close () {
+      close() {
         this.visible = false
         document.removeEventListener('click', this.onClickDocument)
       },
-      onClick (event) {
+      onClick(event) {
         if (this.$refs.triggerWrapper.contains(event.target)) {
           if (this.visible === true) {
             this.close()
@@ -53,6 +56,8 @@
 </script>
 
 <style scoped lang="scss">
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
@@ -60,8 +65,31 @@
     }
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
         transform: translateY(-100%);
+        filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.5));
+        background: white;
+        padding: .5em 1em;
+        margin-top: -10px;
+        max-width: 20em;
+        word-break: break-all;
+        &::before, &::after {
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0px;
+            height: 0px;
+            position: absolute;
+            left: 10px;
+        }
+        &::before {
+            border-top-color: black;
+            top: 100%;
+        }
+        &::after {
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
