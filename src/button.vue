83215eeef442360c8933b3d836aa2dacc1451b55
weite122 <949193531@qq.com>
2019-04-17 23:58:27 +0800

    [A]创建官网

--- src/button.vue
+++ src/button.vue
@@ -2,7 +2,7 @@
     <button :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')" class="g-button">
         <g-icon :name="icon" class="icon" v-if="icon && !loading"></g-icon>
         <g-icon class="loading icon" name="loading" v-if="loading"></g-icon>
-        <div class="content">
+        <div class="g-button-content">
             <slot></slot>
         </div>
     </button>
@@ -33,62 +33,34 @@
 </script>
 
 <style lang="scss" scoped>
+    $font-size: 14px;
+    $button-height: 32px;
+    $button-bg: white;
+    $button-active-bg: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
     @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
     }
-
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
+    .g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
+        border-radius: $border-radius; border: 1px solid $border-color;
+        background: $button-bg;
+        display: inline-flex; justify-content: center; align-items: center;
         vertical-align: middle;
-
-        > .icon {
-            order: 1;
-            margin-right: .3em;
-        }
-
-        > .content {
-            order: 2;
-        }
-
+        &:hover { border-color: $border-color-hover; }
+        &:active { background-color: $button-active-bg; }
+        &:focus { outline: none; }
+        > .g-button-content { order: 2; }
+        > .icon { order: 1; margin-right: .1em; }
         &.icon-right {
-            > .icon {
-                order: 2;
-                margin-right: 0;
-                margin-left: .3em;
-            }
-
-            > .content {
-                order: 1;
-            }
+            > .g-button-content { order: 1; }
+            > .icon { order: 2; margin-right: 0; margin-left: .1em;}
         }
-
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-
-        &:focus {
-            outline: none;
-        }
-
         .loading {
-            animation: spin 1s infinite linear;
+            animation: spin 2s infinite linear;
         }
     }
 
--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -62,6 +62,7 @@
             display: flex;
             align-items: center;
             padding: 0 8px;
+            background: lighten($grey, 8%);
         }
 
         &:first-child {
