75e9289ba45f4d4417a612c874ebd394edebe854
weite122 <949193531@qq.com>
2020-03-05 15:59:41 +0800

    [A]重构validate代码，添加多个条件测试，自定义测试

--- tests/unit/validate.spec.js
+++ tests/unit/validate.spec.js
@@ -95,4 +95,50 @@ describe('validate', () => {
     expect(errors.email.minLength).to.exist
     expect(errors.email.pattern).to.exist
   })
+
+  it('maxLength', () => {
+    let data = {
+      email: '1354324321546'
+    }
+    let rules = [
+      {key: 'email', pattern: 'email', maxLength: 10}
+    ]
+    let errors = validate(data, rules)
+    expect(errors.email.maxLength).to.exist
+  })
+  it('many keys', () => {
+    let data = {
+      email: '123123123123'
+    }
+    let rules = [
+      {
+        key: 'email', required: true, minLength: 5, maxLength: 10,
+        hasNumber: true, hasLowerCaseAndUpperCase: true, hasDot: true, hasUnderscore: true,
+        hasFrank: true
+      }
+    ]
+    let fn = () => {
+      validate(data, rules)
+    }
+    expect(fn).to.throw()
+  })
+  it('自定义测试规则 hasNumber', () => {
+    let data = {
+      email: 'abcabcabcabc'
+    }
+    validate.hasNumber = (value) => {
+      if (!/\d/.test(value)) {
+        return '必须含有数字'
+      }
+    }
+    let rules = [{key: 'email', required: true, hasNumber: true}]
+    let errors
+    let fn = () => {
+      errors = validate(data, rules)
+    }
+    expect(fn).to.not.throw()
+    expect(errors.email.hasNumber).to.eq('必须含有数字')
+  })
+
+
 })
