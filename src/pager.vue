4f02a8912f5e493ccce724ed1e059c23889e8f3f
weite122 <weite122@foxmail.com>
2020-04-11 23:19:51 +0800

    [A]完成pager基本样式

--- src/pager.vue
+++ src/pager.vue
@@ -1,88 +1,98 @@
 <template>
   <div class="wheel-pager">
-    <span v-for="page in pages"
-          class="wheel-pager-item"
-          :class="{active:page === currentPage, separator: page ==='...'}">
-      {{page}}
+    <span class="wheel-pager-nav prev" :class="{disabled:currentPage===1}">
+      <g-icon name="left"></g-icon>
+    </span>
+    <template v-for="page in pages">
+      <template v-if="page === currentPage">
+        <span class="wheel-pager-item current">{{page}}</span>
+      </template>
+      <template v-else-if="page === '...'">
+        <g-icon class="wheel-pager-separator" name="dots"></g-icon>
+      </template>
+      <template v-else>
+        <span href="#" class="wheel-pager-item other">{{page}}</span>
+      </template>
+    </template>
+    <span class="wheel-pager-nav next" :class="{disabled: currentPage===totalPage}">
+      <g-icon name="right"></g-icon>
     </span>
   </div>
 </template>
 
 <script>
-export default {
-  name: "WheelPager",
-  props: {
-    totalPage: {
-      type: Number,
-      required: true
-    },
-    currentPage: {
-      type: Number,
-      required: true
-    },
-    hideIfOnePage: {
-      type: Boolean,
-      default: true
-    }
-  },
-  data() {
-    let pages = unique(
-      [
-        1,
-        this.totalPage,
-        this.currentPage,
-        this.currentPage - 1,
-        this.currentPage - 2,
-        this.currentPage + 1,
-        this.currentPage + 2
-      ].sort((a, b) => a - b)
-    ).reduce((prev, current, index, array) => {
-      prev.push(current);
-      array[index + 1] !== undefined &&
-        array[index + 1] - array[index] > 1 &&
-        prev.push("...");
-      return prev;
-    }, []);
-    return {
-      pages
-    };
-  }
-};
+  import GIcon from './icon'
 
-function unique(array) {
-  // return [...new Set(array)]
-  const object = {};
-  array.map(number => {
-    object[number] = true;
-  });
-  return Object.keys(object).map(s => parseInt(s, 10));
-}
+  export default {
+    name: "WheelPager",
+    components: {GIcon},
+    props: {
+      totalPage: {
+        type: Number,
+        required: true
+      },
+      currentPage: {
+        type: Number,
+        required: true
+      },
+      hideIfOnePage: {
+        type: Boolean,
+        default: true
+      }
+    },
+    data() {
+      let pages = unique([1, this.totalPage,
+        this.currentPage,
+        this.currentPage - 1, this.currentPage - 2,
+        this.currentPage + 1, this.currentPage + 2]
+        .sort((a, b) => a - b))
+        .filter((n) => n >= 1 && n <= this.totalPage)
+        .reduce((prev, current, index, array) => {
+          prev.push(current);
+          array[index + 1] !== undefined &&
+          array[index + 1] - array[index] > 1 &&
+          prev.push("...");
+          return prev;
+        }, []);
+      return {
+        pages
+      };
+    }
+  };
+
+  function unique(array) {
+    // return [...new Set(array)]
+    const object = {};
+    array.map(number => {
+      object[number] = true;
+    });
+    return Object.keys(object).map(s => parseInt(s, 10));
+  }
 </script>
 
 <style scoped lang="scss">
-@import "_var.scss";
-.wheel-pager {
-  &-item {
-    border: 1px solid #e1e1e1;
-    border-radius: $border-radius;
-    padding: 0 4px;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    font-size:12px;
-    min-width: 20px;
-    height: 20px;
-    margin: 0 4px;
-    cursor: pointer;
-    &.separator{
-    border: none;
-  }
-    &.active, &:hover{
-      border-color: $green;
+  @import "_var.scss";
+  .wheel-pager { display: flex; justify-content: flex-start; align-items: center;
+    $width: 20px;
+    $height: 20px;
+    $font-size: 12px;
+    &-separator {
+      width: $width;
+      font-size: $font-size;
     }
-    &.active{
-      cursor: default;
+    &-item {
+      min-width: $width; height: $height;font-size: $font-size;
+      border: 1px solid #e1e1e1; border-radius: $border-radius; padding: 0 4px; display: inline-flex; justify-content: center;
+      align-items: center; margin: 0 4px; cursor: pointer;
+      &.current, &:hover { border-color: $green; }
+      &.current { cursor: default; }
+    }
+    &-nav {
+      margin: 0 4px; display: inline-flex; justify-content: center; align-items: center;
+      background: $grey; height: $height; width: $width; border-radius: $border-radius; font-size: $font-size;
+      &.disabled {
+        svg { fill: darken($grey, 30%); }
+      }
     }
   }
-}
 </style>
