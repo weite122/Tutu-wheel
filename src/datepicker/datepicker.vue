247df9384e9a86b83c24ed4890ccb8f280288d87
weite122 <weite122@foxmail.com>
2020-12-21 16:57:02 +0800

    [A]优化获取42天日期的方法

--- src/datepicker/datepicker.vue
+++ src/datepicker/datepicker.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="wheel-date-picker" style="border: 1px solid #66CC99" ref="wrapper">
-    <g-popover position="bottom" :container="x">
+    <g-popover position="bottom" :container="popoverContainer">
       <g-input type="text"/>
       <template slot="content">
         <div class="wheel-date-picker-pop">
           <div class="wheel-date-picker-nav">
-            <span><g-icon name="leftleft"/></span>
-            <span><g-icon name="left"/></span>
+            <span :class="c(prevYear)"><g-icon name="leftleft"/></span>
+            <span :class="c(prevMonth)"><g-icon name="left"/></span>
             <span @click="onClickYears">2020年</span>
             <span @click="onClickMonths">12月</span>
-            <span><g-icon name="right"/></span>
-            <span><g-icon name="rightright"/></span>
+            <span :class="nextMonth"><g-icon name="right"/></span>
+            <span :class="nextYear"><g-icon name="rightright"/></span>
           </div>
           <div class="wheel-date-picker-panels">
             <div v-if="mode==='year'" class="wheel-date-picker-content">年</div>
             <div v-else-if="mode==='month'" class="wheel-date-picker-content">月</div>
             <div v-else class="wheel-date-picker-content">
               <div :class="c('weekdays')">
-                <span v-for="i in helper.range(0,7)">{{ weekdays[i] }}</span>
+                <span :class="c('weekday')" v-for="i in helper.range(0,7)" :key="i">{{ weekdays[i] }}</span>
               </div>
-              <div :class="c('row')" v-for="i in helper.range(1,7)">
-                <span :class="c('col')" v-for="j in helper.range(1,8)">
+              <div :class="c('row')" v-for="i in helper.range(1,7)" :key=i>
+                <span :class="c('cell')" v-for="j in helper.range(1,8)" :key="j">
                   {{ visibleDays[(i - 1) * 7 + j - 1].getDate() }}
                 </span>
               </div>
@@ -54,12 +54,12 @@ export default {
       mode: 'days',
       value: new Date(),
       helper: helper,
-      x: null,
+      popoverContainer: null,
       weekdays: ['一', '二', '三', '四', '五', '六', '日']
     }
   },
   mounted() {
-    this.x = this.$refs.wrapper
+    this.popoverContainer = this.$refs.wrapper
   },
   methods: {
     onClickYears() {
@@ -79,23 +79,12 @@ export default {
       let last = helper.lastDayOfMonth(date)
       let array = []
       let [year, month, day] = helper.getYearMonthDate(date)
-      for (let i = first.getDate(); i <= last.getDate(); i++) {
-        array.push(new Date(year, month, i))
+      let n = first.getDay()
+      let x = first - (n === 0 ? 6 : n - 1) * 3600 * 24 * 1000
+      for (let i = 0; i < 42; i++) {
+        array.push(new Date(x + i * 3600 * 24 * 1000))
       }
-      let n = first.getDay() === 0 ? 6 : first.getDay() - 1
-      let array2 = []
-      for (let i = 0; i < n; i++) {
-        array2.push(new Date(year, month, -i))
-      }
-      array2 = array2.reverse()
-      let m = 42 - array.length - array2.length
-      let array3 = []
-      for (let i = 1; i <= m; i++) {
-        array3.push(new Date(year, month + 1, i))
-      }
-
-      return [...array2, ...array, ...array3]
-
+      return array
     }
   }
 }
@@ -105,6 +94,14 @@ export default {
 .wheel-date-picker {
   &-nav {
     background: #2a8a5e;
+  }
+  &-cell, &-weekday{
+    width: 32px;
+    height: 32px;
+    //border: 1px solid #2a8a5e;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
   }
   /deep/ .wheel-popover-content-wrapper {
     padding: 0;
