c3a3250ff521534e215d68460772f8107c5689ca
weite122 <weite122@foxmail.com>
2020-12-24 15:01:50 +0800

    [A]优化切换选择年月和选择日的样式

--- src/datepicker/datepicker.vue
+++ src/datepicker/datepicker.vue
@@ -4,30 +4,36 @@
       <g-input type="text" :value="formattedValue"/>
       <template slot="content">
         <div class="wheel-date-picker-pop">
-          <div class="wheel-date-picker-nav">
+          <div class="wheel-date-picker-nav" @selectstart.prevent>
             <span @click="onClickPrevYear" :class="c('prevYear','navItem')"><g-icon name="leftleft"/></span>
             <span @click="onClickPrevMonth" :class="c('prevMonth','navItem')"><g-icon name="left"/></span>
-            <span :class="c('yearAndMonth')">
-              <span @click="onClickYears">{{ display.year }}年</span>
-              <span @click="onClickMonths">{{ display.month + 1 }}月</span>
+            <span :class="c('yearAndMonth')" @click="onClickMonth">
+              <span>{{ display.year }}年</span>
+              <span>{{ display.month + 1 }}月</span>
             </span>
             <span @click="onClickNextMonth" :class="c('nextMonth','navItem')"><g-icon name="right"/></span>
             <span @click="onClickNextYear" :class="c('nextYear','navItem')"><g-icon name="rightright"/></span>
           </div>
           <div class="wheel-date-picker-panels">
-            <div v-if="mode==='year'" class="wheel-date-picker-content">年</div>
-            <div v-else-if="mode==='month'" class="wheel-date-picker-content">月</div>
-            <div v-else class="wheel-date-picker-content">
-              <div :class="c('weekdays')">
-                <span :class="c('weekday')" v-for="i in helper.range(0,7)" :key="i">{{ weekdays[i] }}</span>
-              </div>
-              <div :class="c('row')" v-for="i in helper.range(1,7)" :key=i>
+            <div class="wheel-date-picker-content">
+              <template v-if="mode==='month'">
+                <div :class="c('selectMonth')">
+                  选择年和月
+                </div>
+
+              </template>
+              <template v-else>
+                <div :class="c('weekdays')">
+                  <span :class="c('weekday')" v-for="i in helper.range(0,7)" :key="i">{{ weekdays[i] }}</span>
+                </div>
+                <div :class="c('row')" v-for="i in helper.range(1,7)" :key=i>
                 <span @click="onClickCell(getVisibleDay(i,j))"
                       :class="[c('cell'),{currentMonth:isCurrentMonth(getVisibleDay(i, j))}]"
                       v-for="j in helper.range(1,8)" :key="j">
                   {{ getVisibleDay(i, j).getDate() }}
                 </span>
-              </div>
+                </div>
+              </template>
             </div>
           </div>
           <div class="wheel-date-picker-actions">
@@ -89,11 +95,13 @@ export default {
     getVisibleDay(row, col) {
       return this.visibleDays[(row - 1) * 7 + col - 1]
     },
-    onClickYears() {
-      this.mode = 'year'
-    },
-    onClickMonths() {
-      this.mode = 'month'
+    onClickMonth() {
+      if (this.mode !== 'month') {
+        this.mode = 'month'
+      } else {
+        this.mode = 'days'
+      }
+
     },
     c(...classNames) {
       return classNames.map(className => `wheel-date-picker-${className}`)
@@ -166,6 +174,10 @@ export default {
     &.currentMonth {
       color: black;
     }
+  }
+  &-selectMonth{
+    width: 224px;
+    height: 224px;
   }
   /deep/ .wheel-popover-content-wrapper {
     padding: 0;
